324380c959a38f8372c0343c9fd9ebc2f2c76a6d
Usuario <Usuario@CPD03-M10>
2024-03-04 15:56:10 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace Trabalho
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "0";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button disparador = (Button)sender;
+
+            disparador.IsEnabled = false;
+
+            if(vez == "0")
+            {
+                disparador.Text = "O";
+                vez = "X";
+            }
+            else
+            {
+                disparador.Text="X";
+                vez = "O";
+            }
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
